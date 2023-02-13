554e2af9b0d8e9e90304199b587faba588016475
Konstantin Honcharov <k.honcharov@gmail.com>
2023-02-13 16:41:59 +0300

    feat: add header

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 import './style.scss';
+
+const btnMenu = document.querySelector('.nav__menu-btn') as HTMLButtonElement;
+
+btnMenu.addEventListener('click', () => {
+  btnMenu.classList.toggle('nav__menu-btn_active');
+});
