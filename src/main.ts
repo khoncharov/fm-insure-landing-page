593ecc0e2cda966f749a15e0d524af0eaadf5c44
Konstantin Honcharov <k.honcharov@gmail.com>
2023-02-16 13:33:46 +0300

    feat: add mobile menu

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import './style.scss';
 
 const btnMenu = document.querySelector('.nav__menu-btn') as HTMLButtonElement;
+const menu = document.querySelector('.mobile-nav') as HTMLDivElement;
 
 btnMenu.addEventListener('click', () => {
   btnMenu.classList.toggle('nav__menu-btn_active');
+  menu.classList.toggle('mobile-nav_opened');
+
+  if (menu.classList.contains('mobile-nav_opened')) {
+    menu.setAttribute('aria-expanded', 'true');
+  } else {
+    menu.setAttribute('aria-expanded', 'false');
+  }
 });
